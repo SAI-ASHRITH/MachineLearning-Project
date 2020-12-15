dda5ddc835a1911f5799438a4ad4c87d8f1dd33f
SAI-ASHRITH <ashrith97@gmail.com>
2020-12-14 16:06:58 -0800

    Upload the project code and material.

---
+++ training_espcnn.py
@@ -0,0 +1,94 @@
+import sys
+import keras
+import cv2
+import numpy
+import matplotlib
+import skimage
+from tensorflow.keras import layers
+import tensorflow as tf
+
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from skimage.measure import compare_ssim as ssim
+from keras import backend as K
+from keras.callbacks import LearningRateScheduler
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+import math
+import os
+import random
+
+
+def train_ESPCNN():
+    espcnn=get_model()
+    random.seed()
+    degraded=[]
+    ref=[]
+
+    count=0
+    for file in os.listdir('../input/train-bsd/'):
+        
+        ref_e = cv2.imread('../input/train-bsd/{}'.format(file))
+        ref_e = cv2.cvtColor(ref_e, cv2.COLOR_BGR2YUV)
+        #print(np.shape(ref_e))
+        ref_e=ref_e[:,:,0]
+        ref_e=ref_e[10:310,10:310]
+
+        h = ref_e.shape[0]
+        w = ref_e.shape[1]
+        
+        new_height = h // 3
+        new_width = w // 3
+        #print(w)
+        #print(new_width)
+        deg_e=cv2.resize(ref_e,(new_width,new_height),cv2.INTER_LINEAR)
+ 
+        print(np.shape(ref_e))
+        print(np.shape(deg_e))
+        ref_e=ref_e.astype(float)/255
+        deg_e=deg_e.astype(float)/255
+        temp2=np.zeros((np.shape(ref_e)[0],np.shape(ref_e)[1],1))
+        temp1=np.zeros((np.shape(deg_e)[0],np.shape(deg_e)[1],1))
+        temp1[:,:,0]=deg_e
+        temp2[:,:,0]=ref_e
+        
+        degraded.append(temp1)
+        ref.append(temp2)
+
+        count+=1
+   
+    count=0       
+    print(np.shape(degraded))
+    print(np.shape(ref))
+    randd=list(zip(degraded,ref)) 
+    random.shuffle(randd)
+    degraded,ref=zip(*randd)    
+
+    early_stopping_callback = keras.callbacks.EarlyStopping(monitor="loss", patience=10)
+    loss_fn = keras.losses.MeanSquaredError()
+    optimizer = Adam(learning_rate=0.001)
+    callbacks = [ early_stopping_callback]
+    espcnn.compile(optimizer=optimizer, loss="mean_squared_error")
+
+    espcnn.fit(np.array(degraded),np.array(ref),epochs=500,shuffle=True,  verbose=2)
+    espcnn.save("trained_espcnn.h5")    
+    
+def get_model(upscale_factor=3, channels=1):
+    conv_args = {
+        "activation": "relu",
+        "kernel_initializer": "Orthogonal",
+        "padding": "same",
+    }
+    inputs = keras.Input(shape=(None, None, channels))
+    x = layers.Conv2D(64, 5, **conv_args)(inputs)
+    x = layers.Conv2D(64, 3, **conv_args)(x)
+    x = layers.Conv2D(32, 3, **conv_args)(x)
+    x = layers.Conv2D(channels * (upscale_factor ** 2), 3, **conv_args)(x)
+    outputs = tf.nn.depth_to_space(x, upscale_factor)
+
+    return keras.Model(inputs, outputs)    
+
+train_ESPCNN()    
---
+++ training_srcnn.py
@@ -0,0 +1,150 @@
+import sys
+import keras
+import cv2
+import numpy
+import matplotlib
+import skimage
+print('Python: {}'.format(sys.version))
+print('Keras: {}'.format(keras.__version__))
+print('OpenCV: {}'.format(cv2.__version__))
+print('NumPy: {}'.format(numpy.__version__))
+print('Matplotlib: {}'.format(matplotlib.__version__))
+print('Scikit-Image: {}'.format(skimage.__version__))
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from skimage.measure import compare_ssim as ssim
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+import math
+import os
+import random
+
+# python magic function, displays pyplot figures in the notebook
+#%matplotlib inline
+# define a function for peak signal-to-noise ratio (PSNR)
+def psnr(target, ref):
+         
+    # assume RGB image
+    target_data = target.astype(float)
+    ref_data = ref.astype(float)
+
+    diff = ref_data - target_data
+    diff = diff.flatten('C')
+
+    rmse = math.sqrt(np.mean(diff ** 2.))
+
+    return 20 * math.log10(255. / rmse)
+
+# define function for mean squared error (MSE)
+def mse(target, ref):
+    # the MSE between the two images is the sum of the squared difference between the two images
+    err = np.sum((target.astype('float') - ref.astype('float')) ** 2)
+    err /= float(target.shape[0] * target.shape[1])
+    
+    return err
+
+# define function that combines all three image quality metrics
+def compare_images(target, ref):
+    scores = []
+    scores.append(psnr(target, ref))
+    scores.append(mse(target, ref))
+    scores.append(ssim(target, ref, multichannel =True))
+    
+    return scores
+
+# prepare degraded images by introducing quality distortions via resizing
+
+
+
+# define the SRCNN model
+def model():
+    
+    # define model type
+    SRCNN = Sequential()
+    
+    # add model layers
+    SRCNN.add(Conv2D(filters=128, kernel_size = (9, 9), kernel_initializer='glorot_uniform',
+                     activation='relu', padding='valid', use_bias=True, input_shape=(33, 33, 1)))
+    SRCNN.add(Conv2D(filters=64, kernel_size = (3, 3), kernel_initializer='glorot_uniform',
+                     activation='relu', padding='same', use_bias=True))
+    #SRCNN.add(Conv2D(filters=32, kernel_size = (3, 3), kernel_initializer='glorot_uniform',
+    #                 activation='relu', padding='same', use_bias=True))                 
+    SRCNN.add(Conv2D(filters=1, kernel_size = (5, 5), kernel_initializer='glorot_uniform',
+                     activation='linear', padding='valid', use_bias=True))
+    
+    # define optimizer
+    adam = Adam(lr=0.0003)
+    
+    # compile model
+    SRCNN.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
+    
+    return SRCNN
+
+
+# define necessary image processing functions
+
+def modcrop(img, scale):
+    tmpsz = img.shape
+    sz = tmpsz[0:2]
+    sz = sz - np.mod(sz, scale)
+    img = img[0:sz[0], 0:sz[1]]
+    return img
+
+
+def shave(image, border):
+    img = image[border: -border, border: -border]
+    return img
+
+# define main prediction function
+
+
+
+def train():
+    srcnn=model()
+    degraded_vali=[]
+    ref_vali=[]
+    degraded=[]
+    ref=[]
+    count=0
+    for file in os.listdir('../input/train-bsd'):
+        
+        ref_e = cv2.imread('../input/train-bsd/{}'.format(file))
+        ref_e = cv2.cvtColor(ref_e, cv2.COLOR_BGR2YCrCb)
+        ref_e=ref_e[:,:,0]
+        #ref_e=cv2.normalize(ref_e[:,:,0], None, 0.0, 1.0, cv2.NORM_MINMAX)
+        ref_e=modcrop(ref_e,3)
+        h = ref_e.shape[0]
+        w = ref_e.shape[1]
+        new_height = h // 2
+        new_width = w // 2
+ 
+        deg_e=cv2.resize(cv2.resize(ref_e,(new_width,new_height)),(w,h))
+        ref_e=ref_e.astype(float)/255
+        deg_e=deg_e.astype(float)/255
+        temp1=np.zeros((33,33,1))
+        temp2=np.zeros((21,21,1))
+ 
+        for x in range(0,ref_e.shape[0]-33,14):
+            for y in range(0,ref_e.shape[1]-33,14):
+                temp1[:,:,0]=deg_e[x:x+33,y:y+33]
+                temp2[:,:,0]=ref_e[x+6:x+27,y+6:y+27]
+                
+                degraded.append(temp1)
+                ref.append(temp2)
+                count+=1
+    print(np.shape(degraded)) 
+    print(np.shape(ref)) 
+    count=0       
+
+    randd=list(zip(degraded,ref))
+    random.shuffle(randd)
+    degraded,ref=zip(*randd)        
+
+    srcnn.fit(np.array(degraded),np.array(ref) ,epochs=200,shuffle=True, batch_size=128, verbose=1)
+    srcnn.save("trained_bsd.h5")
+
+train()
+
